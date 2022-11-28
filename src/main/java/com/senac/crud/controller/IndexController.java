2dc1df8d655de0858b25a18ea6bf9e93c876327c
Alysson Oliveira <3oalysson@gmail.com>
2022-11-28 20:27:27 -0300

    ajustes

--- src/main/java/com/senac/crud/controller/IndexController.java
+++ src/main/java/com/senac/crud/controller/IndexController.java
@@ -11,6 +11,6 @@ public class IndexController {
 
     @GetMapping
     public String sayHello() {
-        return "You can create a new Note by making a POST request to /api/notes endpoint.";
+        return "Crie novos requests de Notes usando um POST para o endpoint /api/notes .";
     }
 }
